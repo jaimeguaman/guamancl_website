76aca400abcd232b330b66c6e588b9798f2df29d
Jaime Guaman <jaime@guaman.cl>
2014-09-05 21:03:09 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,181 @@
+'use strict';
+
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		app: 'app',
+		dist: 'dist',
+
+		sass: {
+			options: {
+				includePaths: ['<%= app %>/bower_components/foundation/scss']
+			},
+			dist: {
+				options: {
+					outputStyle: 'compressed'
+				},
+				files: {
+					'<%= dist %>/css/app.min.css': '<%= app %>/scss/app.scss'
+				}
+			},
+			dev: {
+				options: {
+					outputStyle: 'extended'
+				},
+				files: {
+					'<%= app %>/css/app.css': '<%= app %>/scss/app.scss'
+				}
+			}
+		},
+
+		
+
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= app %>/js/**/*.js'
+			]
+		},
+
+		clean: {
+			dist: {
+				src: ['<%= dist %>/*']
+			},
+		},
+		copy: {
+			dist: {
+				files: [{
+					expand: true,
+					cwd:'<%= app %>/',
+					src: ['fonts/**', '**/*.html', '!**/*.scss', '!bower_components/**'],
+					dest: '<%= dist %>/'
+				} , {
+					expand: true,
+					flatten: true,
+					src: ['<%= app %>/bower_components/font-awesome/fonts/**'],
+					dest: '<%= dist %>/fonts/',
+					filter: 'isFile'
+				} ]
+			},
+		},
+
+		imagemin: {
+			target: {
+				files: [{
+					expand: true,
+					cwd: '<%= app %>/images/',
+					src: ['**/*.{jpg,gif,svg,jpeg,png}'],
+					dest: '<%= dist %>/images/'
+				}]
+			}
+		},
+		
+		uglify: {
+			options: {
+				preserveComments: 'some',
+				mangle: false
+			}
+		},
+
+		useminPrepare: {
+			html: ['<%= app %>/index.html'],
+			options: {
+				dest: '<%= dist %>'
+			}
+		},
+
+		usemin: {
+			html: ['<%= dist %>/**/*.html', '!<%= app %>/bower_components/**'],
+			css: ['<%= dist %>/css/**/*.css'],
+			options: {
+				dirs: ['<%= dist %>']
+			}
+		},
+
+		watch: {
+			grunt: {
+				files: ['Gruntfile.js'],
+				tasks: ['sass:dev']
+			},
+			sass: {
+				files: '<%= app %>/scss/**/*.scss',
+				tasks: ['sass:dev']
+			},
+			livereload: {
+				files: ['<%= app %>/**/*.html', '!<%= app %>/bower_components/**', '<%= app %>/js/**/*.js', '<%= app %>/css/**/*.css', '<%= app %>/images/**/*.{jpg,gif,svg,jpeg,png}'],
+				options: {
+					livereload: true
+				}
+			}
+		},
+
+		connect: {
+			app: {
+				options: {
+					port: 9000,
+					base: '<%= app %>/',
+					open: true,
+					livereload: true,
+					hostname: '127.0.0.1'
+				}
+			},
+			dist: {
+				options: {
+					port: 9001,
+					base: '<%= dist %>/',
+					open: true,
+					keepalive: true,
+					livereload: false,
+					hostname: '127.0.0.1'
+				}
+			}
+		},
+
+		wiredep: {
+			target: {
+				src: [
+					'<%= app %>/**/*.html'
+				],
+				exclude: [
+					'modernizr',
+					'font-awesome',
+					'jquery-placeholder',
+					'jquery.cookie',
+					'foundation'
+				]
+			}
+		}
+		
+	});
+
+	
+	grunt.loadNpmTasks('grunt-sass');
+	
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-usemin');
+	grunt.loadNpmTasks('grunt-wiredep');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-newer');
+
+	
+	grunt.registerTask('compile-sass-dev', ['sass:dev']);
+	grunt.registerTask('compile-sass-dist', ['sass:dist']);
+	grunt.registerTask('bower-install', ['wiredep']);
+	
+	grunt.registerTask('default', ['compile-sass-dev', 'bower-install', 'connect:app', 'watch']);
+	grunt.registerTask('validate-js', ['jshint']);
+	grunt.registerTask('server-dist', ['connect:dist']);
+	
+	grunt.registerTask('publish', ['clean:dist', 'compile-sass-dist', 'validate-js', 'useminPrepare', 'copy:dist', 'newer:imagemin', 'concat', 'uglify', 'usemin']);
+
+};
---
+++ app/js/app.js
@@ -0,0 +1,163 @@
+'use strict';
+
+var app=angular.module('guamancl',
+	['ngTouch', 'ngRoute' , 'wu.masonry'])
+	.config(['$routeProvider', 
+		function($routeProvider) {
+	
+		$routeProvider.when('/me', {
+			templateUrl: 'views/me.html',
+			activeTab:'me',
+			controller: 'MeController'
+		});
+		$routeProvider.when('/work', {
+			templateUrl: 'views/work.html',
+			controller: 'WorkController',
+			activeTab:'work'
+		});
+		$routeProvider.when('/social-networks', {
+			templateUrl: 'views/social-network.html',
+			activeTab:'social-networks'
+		});
+		$routeProvider.when('/blog', {
+			templateUrl: 'views/blog.html',
+			activeTab:'blog'
+		});
+		$routeProvider.otherwise({redirectTo: '/me'});
+	}]);
+
+//Navigation Controller
+app.controller('NavigationController',['$rootScope','$scope', '$route', function( $rootScope, $scope, $route){
+	$scope.$route=$route;
+}]);
+
+//Me Controller
+app.controller('MeController', [ '$http', '$scope', function( $http, $scope){
+
+	$scope.skills=[];
+
+	$http.jsonp('http://codeivate.com/users/jaimeguaman.json?callback=JSON_CALLBACK').then(function(response){
+		if (response.data && response.data.languages){
+			var langs = response.data.languages;
+			for (var lang in langs){
+				if (lang !== 'Plain Text'){
+					$scope.skills.push(lang);
+				}
+			}
+		}
+	});
+
+}]);
+
+//Work Controller
+app.controller('WorkController', [ '$http', '$scope', function( $http, $scope){
+
+	$scope.projects = [
+	{
+		company : 'Sinbad SpA',
+		brand: 'Sinbad Travel',
+		url : 'http://cl.sinbad.travel',
+		photoSrc: 'http://guaman.cl/images/sinbad.png',
+		occupation: 'Full Stack Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery', 'PHP', 'MySQL']
+	},
+	{
+		company : 'Portal del flete SpA',
+		brand: 'Transportalo',
+		url : 'http://transportalo.cl',
+		photoSrc: 'http://guaman.cl/images/transportalo.png',
+		occupation: 'Full Stack Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery', 'PHP/Cake', 'MySQL']
+	},
+	{
+		company : 'Setmatch S.A',
+		brand: 'Setmatch',
+		url : 'http://guaman.cl/labs/setmatch',
+		photoSrc: 'http://guaman.cl/images/setmatch.png',
+		occupation: 'Front End Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery']
+	},
+	{
+		company : 'Consensus SpA',
+		brand: 'eTrader',
+		url : '',
+		photoSrc: '',
+		occupation: 'Front End Developer',
+		languages: ['HTML/Jinja2', 'CSS/SASS', 'Javascript/Angular', 'Grunt']
+	},
+	{
+		company : 'Consensus SpA',
+		brand: 'Indicadores Económicos',
+		url : 'http://bit.ly/1pyXGJd',
+		photoSrc: '',
+		occupation: 'Front End Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery', 'Chrome Extension']
+	},
+	{
+		company : 'Side Project',
+		brand: 'Alerta Sismos',
+		url : 'http://guaman.cl/app/alerta-sismos',
+		photoSrc: '',
+		occupation: 'Front End Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery', 'Junar API']
+	},
+	{
+		company : 'Side Project',
+		brand: 'GIF Soup',
+		url : 'http://guaman.cl/labs/GIF',
+		photoSrc: '',
+		occupation: 'Front End Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery', 'Tumblr API']
+	},
+	{
+		company : 'Side Project',
+		brand: 'WebGL Globe Earthquakes',
+		url : 'http://guaman.cl/labs/webglglobe-earthquake/',
+		photoSrc: '',
+		occupation: 'Front End Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery', 'WebGL', 'USGS API']
+	},
+	{
+		company : 'Side Project',
+		brand: 'Best Earth Pix',
+		url : 'http://guaman.cl/labs/twitter-ent',
+		photoSrc: '',
+		occupation: 'Front End Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery', 'Twitter API']
+	},
+	{
+		company : 'Side Project',
+		brand: 'Love Counter',
+		url : 'http://guaman.cl/labs/mes',
+		photoSrc: '',
+		occupation: 'Front End Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery']
+	},
+	{
+		company : 'Side Project',
+		brand: 'Impress.js Demo',
+		url : 'http://guaman.cl/labs/impressjs',
+		photoSrc: '',
+		occupation: 'Front End Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery', 'Flickr API']
+	},
+	{
+		company : 'Side Project',
+		brand: 'Scrolleable Overlay',
+		url : 'http://guaman.cl/labs/scrolleable-overlay',
+		photoSrc: '',
+		occupation: 'Front End Developer',
+		languages: ['HTML', 'CSS', 'Javascript/jQuery', 'UX']
+	},
+	{
+		company : 'Side Project',
+		brand: 'Youjizz Downloader [NSFW]',
+		url : 'http://guaman.cl/app/youjizzdownloader',
+		photoSrc: '',
+		occupation: 'Front End Developer',
+		languages: ['HTML', 'CSS', 'Javascript', 'Chrome Extension']
+	},
+	];
+
+}]);
+
